bc3bcb233ea3a0fe5a2d8eded05e1955dd09a9b4
Linh Nguyen <linhngchi@live.com>
2018-01-31 18:24:58 +0700

    Fix react native warning on load module

--- ReactNative/ios/RCTAuroraIMUI/RCTAuroraIMUIModule.m
+++ ReactNative/ios/RCTAuroraIMUI/RCTAuroraIMUIModule.m
@@ -43,6 +43,14 @@ RCT_EXPORT_MODULE();
                                                body:nil];
 }
 
+- (dispatch_queue_t)methodQueue {
+    return dispatch_get_main_queue();
+}
+
++ (BOOL)requiresMainQueueSetup {
+    return YES;
+}
+
 RCT_EXPORT_METHOD(appendMessages:(NSArray *)messages) {
   [[NSNotificationCenter defaultCenter] postNotificationName:kAppendMessages object: messages];
 }
